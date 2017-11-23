39b823d6c38a58643e0d319e8d043ffecebfffbd
jsalamat <salamat_jon@yahoo.com>
2017-11-22 19:15:19 -0500

    more es6 object notes 2

--- assets/javascript/8_objectsB.js
+++ assets/javascript/8_objectsB.js
@@ -18,7 +18,7 @@ restaurant['appetizers'] = ['Fried Calamari', 'Bruschetta', 'Caprese Salad'];
 restaurant.desserts = ['Homemade Tiramisu', 'Canolli', 'Cheesecake'];
 
 console.log(restaurant);
-
+console.log('__________________________________________________________________________');
 //---------------------------------Methods---------------------------------
 //-------------------------------------------------------------------------
 
@@ -36,9 +36,10 @@ const restaurantB = {
 };
 
 console.log(restaurantB.openRestaurantB());
-
+// Unlock the door, flip the open sign. We are open for business!
 console.log(restaurantB.closeRestaurantB());
-
+// Lock the door, flip the open sign. We are closed.
+console.log('__________________________________________________________________________');
 //---------------------------------Methods: ES6---------------------------
 //------------------------------------------------------------------------
 
@@ -57,8 +58,141 @@ const restaurantC = {
 };
 
 console.log(restaurantC.openRestaurantC());
-
+// Unlock the ES6 door, flip the open sign. We are open for business!
 console.log(restaurantC.closeRestaurantC());
+// Lock the ES6 door, flip the open sign. We are closed.
+console.log('__________________________________________________________________________');
+//-------------------------- this Keyword I------------------------------
+//------------------------------------------------------------------------
 
+const restaurantD = {
+  name: 'Italian Bistro',
+  seatingCapacity: 120,
+  hasDineInSpecial: true,
+  entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine pesto'],
+  openRestaurantD: function() {
+
+  	 // if (hasDineInSpecial) { - hasDineInSpecial is not defined 
+  	 //	because hasDineInSpecial is out of the .openRestaurantD() method's scope.
+    if (this.hasDineInSpecial) {
+      return 'Unlock the THIS door, post the special on the board, then flip the open sign.'
+    } else {
+      return 'Unlock the THIS door, then flip the open sign.'
+    }
+  }
+}
+
+console.log(restaurantD.openRestaurantD());
+// Unlock the THIS door, post the special on the board, then flip the open sign.
+console.log('__________________________________________________________________________');
+//-------------------------- this Keyword II-----------------------------
 //------------------------------------------------------------------------
+
+let myObj = {
+  name: 'Miti',
+  sayHello() {
+    return `${this.name} says hello!`;
+  }
+};
+
+let yourObj = {
+  name: 'Timer'
+};
+
+yourObj.sayHello = myObj.sayHello;
+// Sets the sayHello method on yourObj to be the sayHello method on yourObj
+
+console.log(myObj.sayHello());
+// Miti says hello!
+console.log(yourObj.sayHello());
+// Timer says hello!
+console.log('__________________________________________________________________________');
+//--------------------------Getters and Setters I-------------------------
 //------------------------------------------------------------------------
+
+let restaurantE = {
+  _name: 'Italian Bistro',
+  _seatingCapacity: 120,
+  _hasDineInSpecial: true,
+  _entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine pesto'],
+
+  set seatingCapacity(newCapacity) {
+      if (typeof newCapacity === 'number') {
+        this._seatingCapacity = newCapacity;
+      console.log(`${newCapacity} is valid input.`);
+    } else {
+        console.log(`Change ${newCapacity} to a number.`)
+    }
+  }
+}
+
+
+console.log('__________________________________________________________________________');
+//--------------------------Getters and Setters II------------------------
+//------------------------------------------------------------------------
+
+restaurantE.seatingCapacity = 'One hundred';
+// Change One hundred to a number.
+restaurantE.seatingCapacity = '100';
+// Change 100 to a number.
+
+console.log('__________________________________________________________________________');
+//--------------------------Getters and Setters III-----------------------
+//------------------------------------------------------------------------
+
+let restaurantF = {
+  _name: 'Italian Bistro',
+  _seatingCapacity: 120,
+  _hasDineInSpecial: true,
+  _entrees: ['Penne alla Bolognese', 'Chicken Cacciatore', 'Linguine pesto'],
+
+  set seatingCapacity(newCapacity) {
+      if (typeof newCapacity === 'number') {
+        this._seatingCapacity = newCapacity;
+    } else {
+        console.log(`Change ${newCapacity} to a number.`)
+    }
+  },
+
+  get seatingCapacity() {
+      console.log(`There are ${this._seatingCapacity} seats at Italian Bistro.`);
+      return this._seatingCapacity;
+  }
+}
+
+restaurantF.seatingCapacity = 150;
+const seats = restaurantF.seatingCapacity;
+// There are 150 seats at Italian Bistro.
+console.log('__________________________________________________________________________');
+//--------------------------Overview--------------------------------------
+//------------------------------------------------------------------------
+
+let person = {
+  _name: 'Lu Xun',
+  _age: 137,
+  
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+      console.log(`${ageIn} is valid input.`)
+    } else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
+  },
+  
+  get age() {
+    console.log(`${this._name} is ${this._age} years old`);
+    return this._age;
+  }
+
+};
+
+person.age = 'Thirty-nine'
+//Invalid input
+
+person.age = 39;
+//39 is valid input
+
+console.log(person.age);
+//39
