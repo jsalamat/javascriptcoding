0ce8df0fd3aa1c8ce81e91ac5d9b2f83222fa10a
jsalamat <salamat_jon@yahoo.com>
2017-11-29 14:31:48 -0500

    finish mealmaker challege set and get in obj is awesome

--- assets/javascript/8_objectsB.js
+++ assets/javascript/8_objectsB.js
@@ -1,4 +1,38 @@
-//------------------------------------------------------------------------
+//---------------------------------Properties---------------------------------
+//-------------------------------------------------------------------------
+// One advantage that bracket notation has over dot notation is that 
+// you can use variables inside the brackets to select the keys of an object.
+
+let meal = 'none';
+var d = new Date(); // get date now
+let time = d.getHours(); // Get the hour (0-23)
+console.log(time);
+// We'll use military time for this example, counting hours 0-23.
+
+const restaurantSpecials = {
+ breakfast: 'The breakfast special is 20% off freshly squeezed orange juice',
+ lunch: 'The lunch special is 10% off appetizers',
+ dinner: '$20 Special Dinner for 2 includes 2 entree and 1 appetizers',
+ none: 'There are no specials currently'
+};
+
+if (time < 11) { // 11 am
+  meal = 'breakfast';
+} else if (time < 15) { // 5 pm
+  meal = 'lunch';
+} else if (time < 17) { // 5 pm
+  meal = 'dinner';
+}
+
+
+console.log(restaurantSpecials[meal]);
+// The meal variable is not a key in the restaurantSpecials object. 
+// Because we are using bracket notation, JavaScript looks at the meal variable's value.
+// Bracket notation is required to use variables to look up keys within an object.
+// It's not possible to use variables like this with dot notation.
+console.log('__________________________________________________________________________');
+//---------------------------------Adding Properties---------------------------------
+//-------------------------------------------------------------------------
 
 const restaurant = {
   name: 'Italian Bistro',
@@ -18,6 +52,7 @@ restaurant['appetizers'] = ['Fried Calamari', 'Bruschetta', 'Caprese Salad'];
 restaurant.desserts = ['Homemade Tiramisu', 'Canolli', 'Cheesecake'];
 
 console.log(restaurant);
+
 console.log('__________________________________________________________________________');
 //---------------------------------Methods---------------------------------
 //-------------------------------------------------------------------------
@@ -82,6 +117,12 @@ const restaurantD = {
   }
 }
 
+// The .openRestaurantD() method in the example above will return a value 
+// The this keyword refers to the current object, 
+// which we use to grab the value saved to hasDineInSpecial
+// this.hasDineInSpecial inside the object is the same as 
+// accessing restaurantD.hasDineInSpecial outside the object
+
 console.log(restaurantD.openRestaurantD());
 // Unlock the THIS door, post the special on the board, then flip the open sign.
 console.log('__________________________________________________________________________');
@@ -135,6 +176,10 @@ restaurantE.seatingCapacity = 'One hundred';
 // Change One hundred to a number.
 restaurantE.seatingCapacity = '100';
 // Change 100 to a number.
+
+// remember to remove quotes because it makes it a string
+restaurantE.seatingCapacity = 100;
+// 100 is valid input.
 
 console.log('__________________________________________________________________________');
 //--------------------------Getters and Setters III-----------------------
