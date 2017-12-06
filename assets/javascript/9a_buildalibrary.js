5fe49b1abb6510fb73e0add5b286f129b22f385a
jsalamat <salamat_jon@yahoo.com>
2017-12-06 17:06:16 -0500

    added buildalibrary and school catalogue

--- assets/javascript/9a_buildalibrary.js
+++ assets/javascript/9a_buildalibrary.js
@@ -1,1 +1,22 @@
-9a_buildalibrary.js
+// Books-'N-Stuff carries three different types of media: books, CDs, and movies. 
+// In this project you will create a parent class named Media 
+// with three subclasses: Book, Movie, and CD. 
+// These three subclasses have the following properties and methods:
+
+// --BOOK--
+// Properties: author (string), title (string), pages (number),
+//  isCheckedOut (boolean, initially false), and ratings (array, initially empty).
+// Getters: all properties have a getter
+// Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
+
+// --MOVIE--
+// Properties: director (string), title (string), runTime (number), 
+// isCheckedOut (boolean, initially false), and ratings (array, initially empty)
+// Getters: all properties have a getter
+// Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
+
+// --CD--
+// Properties: artist (string), title (string), 
+// isCheckedOut (boolean, initially false), and ratings (array, initially empty), songs (array of strings)
+// Getters: all properties have a getter
+// Methods: .getAverageRating(), .toggleCheckOutStatus(), and .addRating()
--- assets/javascript/9b_schoolcatalogue.js
+++ assets/javascript/9b_schoolcatalogue.js
@@ -1,1 +1,25 @@
-9b_schoolcatalogue.js
+// create classes for primary, middle, and high schools. 
+// Because these classes share properties and methods, 
+// each will inherit from a parent School class. 
+// Our parent and three child classes have the following properties, getters, setters, and methods:
+
+// -SCHOOL-
+// Properties: name (string), level (one of three strings: 'primary', 'middle', or 'high'), and numberOfStudents (number)
+// Getters: all properties have getters
+// Setters: the numberOfStudents property has a setter
+// Methods: .quickFacts() and .pickSubstituteTeacher() (this is a static method)
+
+// -PRIMARY-
+// Includes everything in the School class, plus one additional property
+// Properties: pickupPolicy (string)
+
+// -MIDDLE-
+// Does not include any additional properties or methods
+
+// -HIGH-
+// Includes everything in the School class, plus one additional property
+// Properties: sportsTeams (array of strings)
+
+// create the constructor() and getters for the four classes above. 
+// Then, use the setter and methods specifications 
+// in steps five, six, and seven to finish the project.
