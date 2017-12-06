9a11361437a0a14ea5ed8ae380e66177918513ff
jsalamat <salamat_jon@yahoo.com>
2017-12-05 23:57:07 -0500

    inheritance class static method notes with samples

--- assets/javascript/9_classes.js
+++ assets/javascript/9_classes.js
@@ -712,5 +712,18 @@ const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
 nurseOlynyk.takeVacationDays(5);
 console.log(nurseOlynyk.remainingVacationDays);
 
-nurseOlynyk.addCertication('Genetics');
-consol.log(nurseOlynyk.certifications);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+//---------------------------------Static Methods-------------------------------------
+//-----------------------------------------------------------------------------------
+// Sometimes you will want a class to have methods 
+// that aren't available in individual instances,
+// but that you can call directly from the class.
+
+// Take the Date class, for example — you can both 
+// create Date instances to represent whatever date you want, 
+// and call static methods, like Date.now() which 
+// returns the current date, directly from the class. 
+// The .now() method is static, so you can call it directly from the class,
+//  but not from an instance of the class.
