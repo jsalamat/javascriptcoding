5c88b1ba1774b7c2951d5fbf9adf0f017591ba8e
jsalamat <salamat_jon@yahoo.com>
2018-01-08 23:13:23 -0500

    reverse string and factorialize number added

--- assets/jsbasic/set1.js
+++ assets/jsbasic/set1.js
@@ -1,3 +1,5 @@
+// Basic Algorithm
+console.log('Reverse a String')
 // Reverse a String
 
 function reverseString(str) {
@@ -7,3 +9,25 @@ function reverseString(str) {
 reverseString("hello");
 
 // ----------------------------------------------
+console.log('-------------------------------------------------')
+console.log('Factorialize a Number')
+// Factorialize a Number
+
+function factorialize(num) {
+  var factorArray = [];
+  var factorTotal = 1;
+  for (var i= 1; i < num + 1; i++) {
+    factorArray.push(i);
+  }
+  for (var j = 0; j < factorArray.length; j++){
+      factorTotal *= factorArray[j];
+  }
+  return factorTotal;
+}
+
+factorialize(5);
+
+// ----------------------------------------------
+console.log('-------------------------------------------------')
+console.log('TITLE')
+// TITLE
