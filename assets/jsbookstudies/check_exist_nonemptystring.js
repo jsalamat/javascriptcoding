cd898252f25a7381e138cc00cf39de9063ff5eb5
jsalamat <salamat_jon@yahoo.com>
2019-01-04 22:52:08 -0500

    added betterchecker

--- assets/jsbookstudies/check_exist_nonemptystring.js
+++ assets/jsbookstudies/check_exist_nonemptystring.js
@@ -1,22 +1,43 @@
 // check_exist_nonemptystring.js
 // Verify that a variable is defined, is a string , and not empty
+let noString;
+let hasString = "Hello World!";
+let hasInterger = 1;
+let zero = 0;
 
 // simplest testing to check for nonempty strings
 let checker = function(unknownVariable) {
 	console.log("-------------------------------------");
 	console.log(unknownVariable);
 	if (typeof unknownVariable === 'string' && unknownVariable.length > 0) {
-		console.log("true")
+		console.log("true: defined and has a String")
 	} else {
 		console.log("It is either undefined or has no string");
 	}
 	console.log("-------------------------------------");
 };
-let noString;
-let hasString = "Hello World!";
-let hasInterger = 1;
-let zero = 0;
+
 checker(noString);
 checker(hasString);
 checker(hasInterger);
 checker(zero);
+
+// Testing for string, regardless of whether it's a string object or a string literal.
+// Test to ensure the variable isn't null
+let betterChecker = function(unknownVariable) {
+	console.log("-------------------------------------");
+	console.log(unknownVariable);
+	if (((typeof unknownVariable != "undefined" && unknownVariable) && 
+		unknownVariable.length > 0) && 
+		typeof unknownVariable.valueOf() == "string") {
+		console.log("true: defined and has a String")
+	} else {
+		console.log("It is either undefined or has no string");
+	}
+	console.log("-------------------------------------");
+};
+
+betterChecker(noString);
+betterChecker(hasString);
+betterChecker(hasInterger);
+betterChecker(zero);
