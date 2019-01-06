f0a29679721df5d90471790854f2fa304df2ce93
jsalamat <salamat_jon@yahoo.com>
2019-01-05 22:30:49 -0500

    add comments betterchecker typeof valueof

--- assets/jsbookstudies/check_exist_nonemptystring.js
+++ assets/jsbookstudies/check_exist_nonemptystring.js
@@ -5,6 +5,12 @@ let hasString = "Hello World!";
 let hasInterger = 1;
 let zero = 0;
 
+// typeof operator returns the type of variable
+console.log(typeof noString);
+console.log(typeof hasString);
+console.log(typeof hasInterger);
+console.log(typeof zero);
+
 // simplest testing to check for nonempty strings
 let checker = function(unknownVariable) {
 	console.log("-------------------------------------");
@@ -24,6 +30,11 @@ checker(zero);
 
 // Testing for string, regardless of whether it's a string object or a string literal.
 // Test to ensure the variable isn't null
+// - typeof test - used to ensure the variable is defined and isn't null
+// -  length test - to see whether its a string and not empty
+// - valueOF test  - returns a primitive value. So if variable is string object it should return a string literal
+
+
 let betterChecker = function(unknownVariable) {
 	console.log("-------------------------------------");
 	console.log(unknownVariable);
